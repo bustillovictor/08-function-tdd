0f55fcb1daed5f9025e9db8ab1fbf9379e5cecba
Victor Bustillo <victorbustillo279@gmail.com>
2016-10-12 15:55:40 -0500

    finsihed monday night HW

--- tests/learning-arrays/01-same-first-test.js
+++ tests/learning-arrays/01-same-first-test.js
@@ -2,16 +2,16 @@
 
 import sameFirst from '../../app/arrays/same-first';
 
-test('It can check first values', (assert) => {
-  const a = sameFirst(['b', true], ['b', true]);
-  const b = sameFirst(['b', true], ['b', false]);
-  const c = sameFirst(['d', true], ['b', true]);
-  const d = sameFirst(['d', true], ['b', false]);
-  const e = sameFirst(['d', true], ['c', false, 'x']);
+test('It can check first values', (assert)) => {
+  const a = sameFirst(['a', true], ['a', true]);
+  const b = sameFirst(['b', true], ['b', true]);
+  const c = sameFirst(['c', false], ['c', false]);
+  const d = sameFirst(['d', false], ['d', false]);
+  const e = sameFirst(['e', false], ['e', false,]);
 
   assert.strictEqual(a, true, 'It returns true when the first values are the same');
   assert.strictEqual(b, true, 'It returns true when the first values are the same');
-  assert.strictEqual(c, false, 'It returns false when the first values are different');
-  assert.strictEqual(d, false, 'It returns false when the first values are different');
-  assert.strictEqual(e, false, 'It returns false when the first values are different');
-});
+  assert.strictEqual(c, false,'It returns false when the first values are different');
+  assert.strictEqual(d, false,'It returns false when the first values are different');
+  assert.strictEqual(e, false,'It returns false when the first values are different');
+}
--- tests/learning-arrays/02-deep-equal-test.js
+++ tests/learning-arrays/02-deep-equal-test.js
@@ -1,3 +1,5 @@
+
+
 /* eslint-env qunit */
 
 import deepEquals from '../../app/arrays/deep-equal';
